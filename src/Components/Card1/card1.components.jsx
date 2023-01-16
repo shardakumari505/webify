884c534105a81e3c507eb3ade86921fcf012e011
sharda kumari <shardakumari505@gmail.com>
2023-01-17 00:55:02 +0530

    Designing movie page

--- src/Components/Card1/card1.components.jsx
+++ src/Components/Card1/card1.components.jsx
@@ -7,10 +7,10 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 const Card1 = (title) => {
  return(<>
     <div className="card-container">
-      <div className="card-top-mentions">
+      {/* <div className="card-top-mentions">
          <FontAwesomeIcon className="rating-star-icon" icon={faStar} />
          <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1"><a href="https://www.imdb.com/title/tt0468569/?ref_=plg_rt_1"><img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" /></a></span>
-      </div>
+      </div> */}
     </div>
  </>)
 }
--- src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
+++ src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
@@ -6,16 +6,20 @@ const DarkKnight = () => {
     return(
         <div className="movie-movie-container">
             <div className="image-container">
-            <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1"><img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" />8.7</span>
+                <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1">
+                    <img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" />
+                    <h6>8.7</h6>
+                </span>
             </div>
+
             <div className="more-movie-movie-container">            
-            <div className="more-text-container"><h1>More like These</h1></div>
-            <div className="movie-movie-cards-container">
-                <Card1/>
-                <Card1/>
-                <Card1/>
-                <Card1/>
-            </div>
+                <div className="more-text-container">More like These</div>
+                <div className="movie-movie-cards-container">
+                    <Card1/>
+                    <Card1/>
+                    <Card1/>
+                    <Card1/>
+                </div>
             </div>
           
 
