68898ca45a3b40dc8e83d26c02316e1d4f4bd131
sharda kumari <shardakumari505@gmail.com>
2023-01-17 23:27:32 +0530

    added details in the movie page

--- src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
+++ src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
@@ -6,9 +6,15 @@ const DarkKnight = () => {
     return(
         <div className="movie-movie-container">
             <div className="image-container">
-                <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1">
-                    <img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" />
-                    <h6>8.7</h6>
+                <div className="movie-title-and-imdb-rating">
+                    <span className="movie-title-on-main-movie-page">The Dark Knight</span>
+                    <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1">
+                        <img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" />
+                        <h6>8.7</h6>
+                    </span>
+                </div>
+                <span className="movie-details-on-main-movie-page">
+                    <span className="meta-data">2008 ‧ U/A ‧ Action ‧ 2h 32m</span>
                 </span>
             </div>
 
