d95d3282cfe3e3aaaff5a72b1d57c8e698b094c1
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-01-20 02:32:04 +0530

    Pushed code

--- src/Components/Movie-Page/movie-page.components.jsx
+++ src/Components/Movie-Page/movie-page.components.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import './movie-page.styles.scss';
 
-const MoviePage = () => {
+const MoviePage = ({MovieName,IMDB,ReleaseYear,Desc,Director,Cast,imgUrl}) => {
     return(
         <div className="movie-movie-container">
-            <div className="image-container">
+            <div className="image-container" style={{backgroundImage:`url(${imgUrl})`}}>
                 <div className="movie-all-texts">
                     <div className="movie-title-and-imdb-rating">
-                        <span className="movie-title-on-main-movie-page">The Dark Knight</span>
+                        <span className="movie-title-on-main-movie-page">{MovieName}</span>
                         <span class="imdbRatingPlugin" data-user="ur161205415" data-title="tt0468569" data-style="p1">
                             <img src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_46x22.png" alt="The Dark Knight (2008) on IMDb" />
-                            <h6>8.7</h6>
+                            <h6>{ IMDB }</h6>
                         </span>
                     </div>
                     <span className="movie-details-on-main-movie-page">
-                        <span className="meta-data">2008 ‧ U/A ‧ Action ‧ English ‧ 2h 32m</span>
+                        <span className="meta-data">{ReleaseYear} ‧ U/A ‧ Action ‧ English ‧ {Duration}</span>
                     </span>
-                    <span className="movie-description-on-main-movie-page">When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.</span>
+                    <span className="movie-description-on-main-movie-page">{Desc}</span>
                     <span className="more-about-movie-on-main-movie-page">
                         <span className="more-meta-data">
                             <span className="left-column">Directors</span>
-                            <span className="right-column">Christopher Nolan</span>
+                            <span className="right-column">{Director}</span>
                         </span>
                         <span className="more-meta-data">
                             <span className="left-column">Starring</span>
-                            <span className="right-column">Christian Bale  Michael Caine  Heath Ledger <br /> Gary Oldman  Aaron Eckhart   Maggie Gyllenhaal <br /> Morgan Freeman</span>
+                            <span className="right-column">{Cast}</span>
                         </span>
                         <span className="more-meta-data">
                             <span className="left-column">Distributed By</span>
