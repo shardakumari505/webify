f03db78f6421ed738dbe795c8784c9012752d9f3
sharda kumari <shardakumari505@gmail.com>
2023-01-22 18:39:21 +0530

    Modified genre card

--- src/Components/genres-card/genres-card.jsx
+++ src/Components/genres-card/genres-card.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './genres-card.styles.scss';
 import { Link } from "react-router-dom";
 
@@ -15,7 +15,7 @@ const GenresCard = () => {
         set3Active(false)
         set4Active(false)
         set5Active(false)
-        
+
     }
     const HandleClick2 = () => {
         set1Active(false)
@@ -81,21 +81,21 @@ const GenresCard = () => {
             </Link> */}
 
             <div className="genres-container">
-                <div className= {is1Active? "genre-panel active":"genre-panel"} onClick={HandleClick1} style={{backgroundImage: `url(https://wallpapers.com/images/hd/horror-wallpaper-wipqucukh5wolwuu.jpg)`}}>
-                    <h3>Horror</h3>
+                <div className={is1Active ? "genre-panel active" : "genre-panel"} onClick={HandleClick1} style={{ backgroundImage: `url(https://wallpapers.com/images/hd/horror-wallpaper-wipqucukh5wolwuu.jpg)` }}>
+                    <Link className="genres-card horror" to="/horrorpage" ><h3 className="genre-title">Horror</h3></Link>
                 </div>
 
-                <div className= {is2Active? "genre-panel active":"genre-panel"} onClick={HandleClick2}  style={{backgroundImage: `url('https://img.freepik.com/free-photo/beautiful-photozone-with-big-wreath-decorated-with-greenery-roses-centerpiece-candles-sides-garland-hanged-trees_8353-11019.jpg?w=2000')`}}>
-                    <h3>Romantic Comedy</h3>
+                <div className={is2Active ? "genre-panel active" : "genre-panel"} onClick={HandleClick2} style={{ backgroundImage: `url('https://img.freepik.com/free-photo/beautiful-photozone-with-big-wreath-decorated-with-greenery-roses-centerpiece-candles-sides-garland-hanged-trees_8353-11019.jpg?w=2000')` }}>
+                    <Link className="genres-card romantic-comedy" to="/romanticcomedypage" ><h3 className="genre-title">Romantic Comedy</h3></Link>
                 </div>
-                <div className={is3Active? "genre-panel active":"genre-panel"} onClick={HandleClick3} style={{backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2wMOKuQsDBuJlhHCiUhwpaNfAh6PORe9nA&usqp=CAU')`}}>
-                    <h3>Mystery</h3>
+                <div className={is3Active ? "genre-panel active" : "genre-panel"} onClick={HandleClick3} style={{ backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2wMOKuQsDBuJlhHCiUhwpaNfAh6PORe9nA&usqp=CAU')` }}>
+                    <Link className="genres-card mystery" to="/mysterypage" ><h3 className="genre-title">Mystery</h3></Link>
                 </div>
-                <div className={is4Active? "genre-panel active":"genre-panel"} onClick={HandleClick4} style={{backgroundImage: `url('https://wallpaperaccess.com/full/5112296.jpg')`}}>
-                    <h3>Anime</h3>
+                <div className={is4Active ? "genre-panel active" : "genre-panel"} onClick={HandleClick4} style={{ backgroundImage: `url('https://wallpaperaccess.com/full/5112296.jpg')` }}>
+                    <Link className="genres-card anime" to="/animepage" ><h3 className="genre-title">Anime</h3></Link>
                 </div>
-                <div className={is5Active? "genre-panel active":"genre-panel"} onClick={HandleClick5} style={{backgroundImage: `url('https://wallpapercave.com/wp/wp3173712.jpg')`}}>
-                    <h3>Action</h3>
+                <div className={is5Active ? "genre-panel active" : "genre-panel"} onClick={HandleClick5} style={{ backgroundImage: `url('https://wallpapercave.com/wp/wp3173712.jpg')` }}>
+                    <Link className="genres-card action" to="/actionpage" ><h3 className="genre-title">Action</h3></Link>
                 </div>
             </div>
 
