1a8f54fa1213f9e4840532b5598515d3f294599b
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-01-22 19:15:03 +0530

    BTHM

--- src/Components/Dropmenu-button/Dropmenu.components.jsx
+++ src/Components/Dropmenu-button/Dropmenu.components.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './Dropmenu.styles.scss';
 
-const Dropmenu = (nextblock) => {
+const Dropmenu = ({nextblock}) => {
     return(
         <div className="dashboard-container-middle">
-            <a className="dropdown" href=#{nextblock}><span className="fa fa-chevron-down chevron"></span></a>
+            <a className="dropdown" href={`#${nextblock}`}><span className="fa fa-chevron-down chevron"></span></a>
         </div>
     )
 }
