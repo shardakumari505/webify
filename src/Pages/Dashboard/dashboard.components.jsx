7e9008389d8983490573479179d2d834fff63e8e
sharda kumari <shardakumari505@gmail.com>
2023-01-22 23:57:31 +0530

    updated

--- src/Pages/Dashboard/dashboard.components.jsx
+++ src/Pages/Dashboard/dashboard.components.jsx
@@ -4,32 +4,13 @@ import GenresCard from "../../Components/genres-card/genres-card";
 import Dropmenu from "../../Components/Dropmenu-button/Dropmenu.components";
 
 const Dashboard = () => {
-    return (<div className="dashboard-container">
-        <div className="dashboard-container-top-top">
-            <div class="ag-format-container">
-                <div class="ag-fire-man_box">
-                    <div class="ag-fire-man_fire-bg">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/man-with-fire/images/fire-bg.png" class="ag-fire-man_box-img" alt="" />
-                    </div>
-                    <div class="ag-fire-man_fire ag-fire-man_fire-1 anim">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/man-with-fire/images/f1.jpg" class="ag-fire-man_fire-img" alt="" />
-                    </div>
-                    <div class="ag-fire-man_fire ag-fire-man_fire-2 anim">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/man-with-fire/images/f2.jpg" class="ag-fire-man_fire-img" alt="" />
-                    </div>
-                    <div class="ag-fire-man_fire ag-fire-man_fire-3 anim">
-                        <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/man-with-fire/images/f3.jpg" class="ag-fire-man_fire-img" alt="" />
-                    </div>
-                    <img src="https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/man-with-fire/images/man.png" alt="" />
-                </div>
-            </div>
+    return (<div className="dashboard-container">            
             <div className="dashboard-container-top">
                 <h1 className="dashboard-title-text">
                     <span className="dashboard-title-text-span">Ci</span>ne<span className="dashboard-title-text-span">Philes</span>
                 </h1>
                 <p className="dashboard-description-text">Everyone wants something to watch when they are bored, so Cinephiles is a one stop solution for movie lovers. We have curated the top five movies across five genres.</p>
             </div>
-        </div>
         {/* <div className="dashboard-container-middle">
             <a className="dropdown" href="#genreCrd"><span className="fa fa-chevron-down chevron"></span></a>
         </div> */}
