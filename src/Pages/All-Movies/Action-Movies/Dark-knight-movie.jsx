d442efc3912d12a87a3b8021db3dfa70b23559a5
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-01-22 22:58:23 +0530

    Added links to cards

--- src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
+++ src/Pages/All-Movies/Action-Movies/Dark-knight-movie.jsx
@@ -13,10 +13,10 @@ const DarkKnight = () => {
                  <div className="more-movie-movie-container" id="more-movie-movie-container">            
                 <div className="more-text-container">More like These</div>
                 <div className="movie-movie-cards-container" id="movie-movie-cards-container">
-                    <div className="card1-container-movie"><Card1 MovieName={LordOfTheRings.MovieName} imgUrl={LordOfTheRings.imgUrl} Desc={LordOfTheRings.Desc} IMDB={LordOfTheRings.IMDB}/></div>
-                    <div className="card2-container-movie"><Card1 MovieName={Inception.MovieName} imgUrl={Inception.imgUrl} Desc={Inception.Desc} IMDB={Inception.IMDB}/></div>
-                    <div className="card3-container-movie"><Card1 MovieName={Matrix.MovieName} imgUrl={Matrix.imgUrl} Desc={Matrix.Desc} IMDB={Matrix.IMDB}/></div>
-                    <div className="card4-container-movie"><Card1 MovieName={StarWars.MovieName} imgUrl={StarWars.imgUrl} Desc={StarWars.Desc} IMDB={StarWars.IMDB}/></div>
+                    <div className="card1-container-movie"><Link className="movie-link" to='/lordoftherings-movie'><Card1 MovieName={LordOfTheRings.MovieName} imgUrl={LordOfTheRings.imgUrl} Desc={LordOfTheRings.Desc} IMDB={LordOfTheRings.IMDB}/></Link></div>
+                    <div className="card2-container-movie"><Link className="movie-link" to='/inception-movie'><Card1 MovieName={Inception.MovieName} imgUrl={Inception.imgUrl} Desc={Inception.Desc} IMDB={Inception.IMDB}/></Link></div>
+                    <div className="card3-container-movie"><Link className="movie-link" to='/matrix-movie'><Card1 MovieName={Matrix.MovieName} imgUrl={Matrix.imgUrl} Desc={Matrix.Desc} IMDB={Matrix.IMDB}/></Link></div>
+                    <div className="card4-container-movie"><Link className="movie-link" to='/starwars-movie'><Card1 MovieName={StarWars.MovieName} imgUrl={StarWars.imgUrl} Desc={StarWars.Desc} IMDB={StarWars.IMDB}/></Link></div>
                 </div>
             </div>
         </div>
