6a74d123ee5314a0750f9b3e7ce0a78012082055
sharda kumari <shardakumari505@gmail.com>
2023-01-22 18:13:38 +0530

    Added details on profile page

--- src/Pages/Profile-page/profile.page.jsx
+++ src/Pages/Profile-page/profile.page.jsx
@@ -17,28 +17,92 @@ const ProfilePage = () => {
                     <img className="sharda-pic" src={Sharda} alt="profile two" />
                 </div>
 
-                <div class="details jane">
-                    <h2>Sharda Kumari
-                        <br />
-                        <span class="job-title">National Institute of Technology , Mizoram</span>
-                    </h2>
+                <div class="details sharda">
+                    <h2>Sharda Kumari</h2>
+
+                    <span class="college">National Institute of Technology , Mizoram</span>
+
+                    <span className="current-job">Upcoming SDE Intern at Microsoft</span>
+                    <span class="profile-social-links">
+                        <a target="_blank" href="https://www.linkedin.com/in/ksharda/">
+                            <i class="fa fa-linkedin my-icon linkedin"></i>
+                        </a>
+                        <a target="_blank" href="https://github.com/shardakumari505">
+                            <i class="fa fa-github my-icon"></i>
+                        </a>
+                        <a target="_blank" href="https://www.instagram.com/sharda2282/">
+
+                            <i class="fa fa-instagram my-icon insta"></i>
+                        </a>
+                        <a target="_blank" href="https://twitter.com/sharda9_singh">
+                            <i class="fa fa-twitter my-icon"></i>
+                        </a>
+                    </span>
+                    <span className="interest">Experiences</span>
+                    <ul className="experience">
+                        <li>Research Fellow at IIT Delhi</li>
+                        <li>Microsoft Engage Mentee</li>
+                        <li>Web Developer at Vaguss</li>
+                        <li>D2C Lead Igniter</li>
+                        <li>Marketing Intern at Shaastra, IIT Madras</li>
+                        <li>Analytics Intern at HH Education</li>
+                    </ul>
+                    <span className="interest">Achievements</span>
+                    <ul className="experience">
+                        <li>Amazon ML Summer School</li>
+                        <li>First Runner Up in WebdevnElan , IIT Hyderabad</li>
+                        <li>Cleared 3rd round in NVCTI , IIT Dhanbad</li>
+                        <li>Second Runner Up in Mixed Medium Art, IIT Bombay</li>
+                    </ul>
                 </div>
             </div>
 
             <div className="profile-card-two">
-                    <div class="pr-card">
-                        <div class="imgBx">
-                            <img src={Sameer} alt="" />
-                        </div>
-
-                        <div class="contentBx">
-
-                            <h2>Sameer Kumar Shrivastava</h2>
-
-                            <span class="job-title">Sitamarhi Institute of Technology , Sitamarhi</span>
-                        </div>
-
+                <div class="pr-card">
+                    <div class="imgBx">
+                        <img src={Sameer} alt="" />
                     </div>
+
+                    <div class="contentBx">
+
+                        <h2>Sameer Kumar Shrivastava</h2>
+
+                        <span class="college">Sitamarhi Institute of Technology , Sitamarhi</span>
+
+                        <span className="current-job">SDE Intern at ELDAAS</span>
+                        <span class="profile-social-links">
+                            <a target="_blank" href="https://www.linkedin.com/in/sameer-kumar-shrivastava/">
+                                <i class="fa fa-linkedin my-icon linkedin"></i>
+                            </a>
+                            <a target="_blank" href="https://github.com/sameer-kumar-shrivastava">
+                                <i class="fa fa-github my-icon"></i>
+                            </a>
+                            <a target="_blank" href="https://www.instagram.com/sameer__shrivastava/">
+
+                                <i class="fa fa-instagram my-icon insta"></i>
+                            </a>
+                            <a target="_blank" href="https://twitter.com/i_sameer21">
+                                <i class="fa fa-twitter my-icon"></i>
+                            </a>
+                        </span>
+                        <span className="interest">Experiences</span>
+                        <ul className="experience">
+                            <li>Ninja Entrepreneur at Coding Ninjas</li>
+                            <li>Web Developer at Vaguss</li>
+                            <li>Club Incharge & Council Member at Coding Club, SIT</li>
+                            <li>Frontend Developer at Executive Strokes</li>
+                            <li>Content Developer at Doubtnut</li>
+                        </ul>
+                        <span className="interest">Achievements</span>
+                        <ul className="experience">
+                            <li>All India Topper of NPTEL Course, IIT Roorkee</li>
+                            <li>Winner of Technical Paper presentation at National Technical Symposium</li>
+                            <li>First Runner Up in webathon at WebdevnElan , IIT Hyderabad</li>
+                            <li>Indigo Squad Member, IIT Bombay</li>
+                        </ul>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
